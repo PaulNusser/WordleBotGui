1247f324579cd87f0ce542259c356cb5deb31a80
PaulNusser <pauli.nusser@gmail.com>
2024-06-02 15:46:04 -0700

    initial screen

--- wordlegame.py
+++ wordlegame.py
@@ -1,14 +1,14 @@
 import pygame
 import sys
 pygame.init()
-SCREEN_WIDTH = 623
+SCREEN_WIDTH = 923
 SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 background_image = pygame.image.load('Pictures/StartingTiles.png')
 background_rect = background_image.get_rect()
 print(background_rect.width, background_rect.height)
 running = True
-center_x = (SCREEN_WIDTH - background_rect.width) / 2
+center_x = 100
 center_y = 0
 while running:
     for event in pygame.event.get():
